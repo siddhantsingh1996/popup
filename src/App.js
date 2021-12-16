2266de1aacde82c37932a468e996d85fb87f2536
siddhantsingh1996 <siddhant.singh@roro.io>
2021-12-16 18:03:15 +0530

    engines add

--- src/App.js
+++ src/App.js
@@ -44,17 +44,20 @@ const Box =styled.div`
   
     
 
-    const math = String.raw`
-    <p><math xmlns="http://www.w3.org/1998/Math/MathML"><msqrt><mi>x</mi><mo>+</mo><mi>b</mi></msqrt></math>Hello this is here</p>
-    `;  
+  
    
-    const [mathValue,setMathValue] = useState('')
+    const [mathValue,setMathValue] = useState('');
+
+    const mathdata= '<p><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><msqrt><mn>2</mn><mi>x</mi></msqrt></math><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mo>∬</mo><mi>x</mi><mo>.</mo><mi>d</mi><mi>x</mi><mo>+</mo><msubsup><mo>∫</mo><mi>b</mi><mi>a</mi></msubsup><mi>a</mi><mo>+</mo><mi>b</mi><mo>.</mo><mi>d</mi><mi>x</mi></math><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><munder><mi>lim</mi><mrow><mn>1</mn><mo>→</mo><mo>∞</mo></mrow></munder><mfrac><mrow><mo>d</mo><mi>a</mi></mrow><mrow><mo>d</mo><mi>b</mi></mrow></mfrac></math><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mo>∯</mo><mi>sin</mi><mfenced><mi>x</mi></mfenced></math><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><msub><mi>log</mi><mn>10</mn></msub><mfenced><mn>2</mn></mfenced></math>+<math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mo>∂</mo><mo>∈</mo><mo>≥</mo><mo>∋</mo><mo>∧</mo><mo>∨</mo><mo>∀</mo><mo>¬</mo><mo>∃</mo><mo>∄</mo><mo>△</mo><mo>○</mo><mo>⊙</mo><mo>⊗</mo><mo>≡</mo><mo>≈</mo><mo>⩾</mo><mi mathvariant=\"normal\">π</mi><mo>∂</mo><mo>∞</mo><mo>∆</mo><mo>∅</mo><mo>+</mo><mo>-</mo><mo>±</mo></math><math xmlns=\"http://www.w3.org/1998/Math/MathML\"><mfenced close=\"|\" open=\"|\"><mtable><mtr><mtd><mi>a</mi></mtd><mtd><mi>b</mi></mtd></mtr><mtr><mtd><mi>c</mi></mtd><mtd><mi>d</mi></mtd></mtr><mtr><mtd><mi>e</mi></mtd><mtd><mi>f</mi></mtd></mtr></mtable></mfenced><mo>+</mo><mfenced><mtable><mtr><mtd><mi>a</mi></mtd><mtd><mi>b</mi></mtd><mtd><mi>c</mi></mtd><mtd><mi>d</mi></mtd></mtr><mtr><mtd><mi>e</mi></mtd><mtd><mi>f</mi></mtd><mtd><mi>g</mi></mtd><mtd><mi>h</mi></mtd></mtr><mtr><mtd><mi>i</mi></mtd><mtd><mi>j</mi></mtd><mtd><mi>k</mi></mtd><mtd><mi>l</mi></mtd></mtr></mtable></mfenced><mo>+</mo><mfenced close=\"]\" open=\"[\"><mtable><mtr><mtd><mi>a</mi></mtd><mtd><mi>b</mi></mtd><mtd><mi>c</mi></mtd><mtd><mi>d</mi></mtd><mtd><mi>e</mi></mtd><mtd><mi>g</mi></mtd></mtr><mtr><mtd><mn>2</mn></mtd><mtd><mn>3</mn></mtd><mtd><mn>4</mn></mtd><mtd><mn>5</mn></mtd><mtd><mn>6</mn></mtd><mtd><mn>7</mn></mtd></mtr></mtable></mfenced></math></p>';
+
+    const math = String.raw`${mathdata}`
     //<MathJax math={math} />
   return (
       <>
         <Container>
             <Box>
               <div className="input-text" onClick={() => openModal()}>{mathValue}</div>
+              {/* <p>{<MathJax math={math} />}</p> */}
               <EditorpopUp showModal={showModal} setShowModal={setShowModal} setMathValue={setMathValue} setText={setText} text={text} />
             </Box>
         </Container>
